49554d970e7f594064d57872dcf4bb08ae97e442
Lepros311 <andy.strickland@gmail.com>
2025-10-15 18:55:13 -0400

    Changed default setup to align with Homes

--- CoffeeTracker.Frontend/src/app/app.ts
+++ CoffeeTracker.Frontend/src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrls: ['./app.css'], // or './app.component.css' if you rename
+  imports: [RouterOutlet, RouterLink],
 })
 export class App {
-  protected readonly title = signal('CoffeeTracker.Frontend');
+  title = 'homes';
 }
