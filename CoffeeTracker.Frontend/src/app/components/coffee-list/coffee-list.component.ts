00a2aa93ca661adae6498bb202e4de1db1e06fd7
Lepros311 <andy.strickland@gmail.com>
2025-10-20 20:22:31 -0400

    Update coffee-list component to integrate coffee-form component

--- CoffeeTracker.Frontend/src/app/components/coffee-list/coffee-list.component.ts
+++ CoffeeTracker.Frontend/src/app/components/coffee-list/coffee-list.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ApiService, PaginationParams} from '../../services/api.service';
 import {CoffeeDto} from '../../models/coffee.model';
+import {CoffeeFormComponent} from './coffee-form/coffee-form.component'
 
 @Component({
     selector: 'app-coffee-list',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CoffeeFormComponent],
     template: `
         <div class="coffee-list">
             <h2>Coffee List</h2>
@@ -35,6 +36,19 @@ import {CoffeeDto} from '../../models/coffee.model';
                     }
                 </div>
             }
+
+            @if (showForm) {
+                <app-coffee-form
+                    [coffee]="selectedCoffee"
+                    [isEditing]="isEditing"
+                    (save)="onSaveCoffee($event)"
+                    (cancel)="onCancelForm()">
+                </app-coffee-form>
+            }
+
+            @if (!showForm) {
+                <button class="add-btn" (click)="addCoffee()">Add New Coffee</button>
+            }
         </div>
     `,
     styleUrls: ['./coffee-list.component.css']
@@ -43,6 +57,9 @@ export class CoffeeListComponent implements OnInit {
     coffees: CoffeeDto[] = [];
     loading = false;
     error: string | null = null;
+    showForm = false;
+    isEditing = false;
+    selectedCoffee: CoffeeDto = {id: 0, name: '', price: 0};
 
     constructor(private apiService: ApiService) {}
 
@@ -72,13 +89,67 @@ export class CoffeeListComponent implements OnInit {
         });
     }
 
+    addCoffee(): void {
+        this.selectedCoffee = {id: 0, name: '', price: 0};
+        this.isEditing = false;
+        this.showForm = true;
+    }
+
     editCoffee(coffee: CoffeeDto): void {
         //TODO: Implement edit functionality
         console.log('Edit coffee: ', coffee);
     }
 
+    onSaveCoffee(coffee: CoffeeDto): void {
+        if (this.isEditing) {
+            this.updateCoffee(coffee);
+        } else {
+            this.createCoffee(coffee);
+        }
+    }
+
+    createCoffee(coffee: CoffeeDto): void {
+        this.apiService.createCoffee(coffee).subscribe({
+            next: () => {
+                this.loadCoffees();
+                this.showForm = false;
+            },
+            error: (err) => {
+                this.error = 'Failed t0 create coffee';
+                console.error('Error creating coffee: ', err);
+            }
+        });
+    }
+
+    updateCoffee(coffee: CoffeeDto): void {
+        this.apiService.updateCoffee(coffee.id, coffee).subscribe({
+            next: () => {
+                this.loadCoffees();
+                this.showForm = false;
+            },
+            error: (err) => {
+                this.error = 'Failed to update coffee';
+                console.error('Error updating coffee: ', err);
+            }
+        });
+    }
+
     deleteCoffee(id: number): void {
-        //TODO: Implement delete functionality
-        console.log('Delete coffee: ', id);
+        if (confirm('Are you sure you want to delete this coffee?')) {
+            this.apiService.deleteCoffee(id).subscribe({
+                next: () => {
+                    this.loadCoffees();
+                },
+                error: (err) => {
+                    this.error = 'Failed to delete coffee';
+                    console.error('Error deleting coffee: ', err);
+                }
+            });
+        }
+    }
+
+    onCancelForm(): void {
+        this.showForm = false;
+        this.selectedCoffee = {id: 0, name: '', price: 0};
     }
 }
